bb180d175453ef58bd29ff0272f0b3b31ce6ffa7
Anton <geraskin.antony@yandex.ru>
2022-12-08 17:51:52 +0700

    BasicAuth test

--- src/test/java/theinternet/BasicAuthTest.java
+++ src/test/java/theinternet/BasicAuthTest.java
@@ -2,11 +2,8 @@ package theinternet;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import scenarios.WebDriverSteps;
 import utils.FileUtils;
 
