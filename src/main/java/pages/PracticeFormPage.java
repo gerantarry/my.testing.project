d4d99d6b72b8bac4f1802ca569b453847cd64f2f
Anton <geraskin.antony@yandex.ru>
2023-01-02 12:36:41 +0700

    checkboxes locators and methods wrote radiobuttons locators and methods wrote

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -14,14 +14,31 @@ public class PracticeFormPage {
     private WebElement lastName;
     @FindBy(id = "userEmail")
     private WebElement userEmail;
-    @FindBy()
-    private WebElement gender;
+    //Gender
+    @FindBy(xpath = "label[text() = 'Male']")
+    private WebElement male;
+    @FindBy(xpath = "label[text() = 'Female']")
+    private WebElement female;
+    @FindBy(id = "gender-radio-3")
+    private WebElement otherGender;
+
     @FindBy(id = "userNumber")
     private WebElement userNumber;
     @FindBy(id = "dateOfBirthInput")
     private WebElement dateOfBirth;
+    //Hobbies
+    @FindBy(id = "hobbies-checkbox-1")
+    private WebElement sport;
+    @FindBy(xpath = "//label[text() = 'Reading']")
+    private WebElement reading;
+    @FindBy(xpath = "label[text() = 'Music']")
+    private WebElement music;
+
     @FindBy(id = "submit")
     private WebElement submit;
+
+    public final static String READING = "Reading", SPORT = "Sport", MUSIC = "Music";
+    public final static String MALE = "Male", FEMALE = "Female", OTHER_GENDER = "Other";
 
     PracticeFormPage(final WebDriver driver){
         PageFactory.initElements(driver, this);
@@ -31,6 +48,42 @@ public class PracticeFormPage {
         firstName.sendKeys(fName);
         lastName.sendKeys(lName);
         submit.submit();
+    }
+
+    /**
+     * Choose and set gender radiobutton
+     * Allowed values: Male, Female, Other. Use public constants of the class.
+     * @param gender for the radiobutton
+     */
+    public void setGender(String gender){
+        switch (gender){
+            case MALE: male.click();
+            break;
+            case FEMALE: female.click();
+            break;
+            case OTHER_GENDER: otherGender.click();
+            default:
+                throw new IllegalArgumentException("parameter 'gender' has incorrect value: " + gender);
+        }
+    }
+
+    /**
+     * Choose and set hobbies checkboxes
+     * Allowed values: Sport, Reading, Music. Use public constants of the class.
+     * @param hobbies for the checkboxes
+     */
+    public void setHobbies(String ... hobbies){
+        for (String hobbie : hobbies){
+            switch (hobbie){
+                case READING: reading.click();
+                break;
+                case SPORT: sport.click();
+                break;
+                case MUSIC: music.click();
+                default:
+                    throw  new IllegalArgumentException("parameter 'hobbie' has incorrect value: " + hobbie);
+            }
+        }
     }
 
 
