482e38546c0178597e1c140f0e8d7b16a49e20a7
Anton <geraskin.antony@yandex.ru>
2023-01-11 21:29:37 +0700

    cucumber runs in mvn test, allure works, but there are no scripts steps in the report

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         features = {"classpath:cucumber/toolsQa/"},
         glue = {"toolsQa"})
 public class RunCucumberTest {
