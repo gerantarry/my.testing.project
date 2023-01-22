3d41fe16f5c8a3fcd86964471d489e916e43a453
Anton <geraskin.antony@yandex.ru>
2023-01-22 14:18:19 +0700

    delete pluginManagement allure-results cant be generated

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         features = {"classpath:cucumber/toolsQa/"},
         glue = {"toolsQa"})
 public class RunCucumberTest {
