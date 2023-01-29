fb3f742d15934522ec2f9750c71c5aa8c928ac1a
Anton <geraskin.antony@yandex.ru>
2023-01-29 19:55:23 +0700

    toJsonString method in Booking.java util for json reading

--- src/main/java/entity/restfullbooker/getbooking/Booking.java
+++ src/main/java/entity/restfullbooker/getbooking/Booking.java
@@ -14,10 +14,23 @@ public class Booking {
     private String additionalneeds;
 
 
-
+    public String toJsonString(){
+        return "{\n" +
+                "  \"firstname\" : \""+firstname+"\",\n" +
+                "  \"lastname\" : \""+ lastname +"\",\n" +
+                "  \"totalprice\" : "+ totalprice +",\n" +
+                "  \"depositpaid\" : "+ depositpaid +",\n" +
+                "  \"bookingdates\" : {\n" +
+                "    \"checkin\" : \""+bookingdates.getCheckin()+"\",\n" +
+                "    \"checkout\" : \""+bookingdates.getCheckout()+"\"\n" +
+                "  },\n" +
+                "  \"additionalneeds\" : \""+ additionalneeds +"\"\n" +
+                "}";
+    }
 }
+
 @Data
-class BookingDates{
+class BookingDates {
     private String checkin;
     private String checkout;
 }
--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -1,6 +1,9 @@
 package utils;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +15,20 @@ final public class FileUtils {
 
     }
 
-    public static final String URLS_FILE_PATH = "src/test/resources/url.properties";
+    public static final String TEST_RESOURCES_ROOT_PATH = "src/test/resources/";
+    public static final String TEST_REQUESTS_PATH = TEST_RESOURCES_ROOT_PATH + "request/";
+    public static final String URLS_FILE_PATH = TEST_RESOURCES_ROOT_PATH + "url.properties";
+
+    /**
+     *
+     * @param fileName without root context
+     * @param classType for transformation
+     * @return T class
+     */
+    public static <T> T readJsonFile(String fileName, Class<T> classType) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+       return mapper.readValue(new File(fileName), classType);
+    }
 
     public static Properties readPropertiesFile(final String path){
         try (InputStream input = new FileInputStream(path)){
